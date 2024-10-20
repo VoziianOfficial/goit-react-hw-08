bf027b2a311d8410c5cad10fa1db561082487f97
Yuliia Voziian <vozianulia@gmail.com>
2024-10-20 12:49:30 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,7 +5,6 @@ import {
 } from "../../redux/auth/selectors";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
-import UserMenu from "../UserMenu/UserMenu";
 import { header, header_container } from "./AppBar.module.css";
 export default function AppBar() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
